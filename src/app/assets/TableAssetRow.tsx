37fb98e82fd0245aac078f40a9ee78b274126ab9
Alexsander <alexsander1996@yahoo.com.br>
2025-03-04 12:08:59 -0300

    gerencia de estado com zustand
    - Criado a gerência de estado com a lib zustand para os ativos;
    - Utilização da gerência do estado nos componentes e páginas;
    - Conversão das linhas das tabelas para componente customizado (evitando transformação de páginas em client e otimizando a performance);

---
+++ src/app/assets/TableAssetRow.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { AssetShow } from "@/components/AssetShow";
+import { Asset } from "@/models";
+import { useAssetStore } from "@/store";
+import { Button, TableCell, TableRow } from "flowbite-react";
+import Link from "next/link";
+import { useShallow } from "zustand/react/shallow";
+
+export default function TableAssetRow(props: {
+  asset: Asset;
+  walletId: string;
+}) {
+  const { asset, walletId } = props;
+
+  const assetFound = useAssetStore(
+    // ? no formato abaixo, faz renderizar toda a lista de ativos, nao somente o registro necessario
+    // state.assets.find((a) => a.symbol === Asset.asset.symbol)
+    // ? no formato abaixo, eh muito mais performatico, pois so renderiza a linha em questao
+    // ? detalhe importante eh que isso ocorre pois sempre geramos um novo array no change do store por causa do principio da imutabilidade
+    useShallow((state) => state.assets.find((a) => a.symbol === asset.symbol))
+  );
+
+  // ? se tiver no estado pega o ativo de la, senao das props
+  const asset_ = assetFound || asset;
+
+  return (
+    <TableRow>
+      <TableCell>
+        <AssetShow asset={asset_} />
+      </TableCell>
+      <TableCell>R$ {asset_.price}</TableCell>
+      <TableCell>
+        <Button
+          className="w-fit"
+          color="light"
+          as={Link}
+          href={`/assets/${asset_.symbol}?wallet_id=${walletId}`}
+        >
+          Comprar/vender
+        </Button>
+      </TableCell>
+    </TableRow>
+  );
+}
--- src/components/AssetsSync.tsx
+++ src/components/AssetsSync.tsx
@@ -2,17 +2,20 @@
 
 import { Asset } from "@/models";
 import { socket } from "@/socket-io";
+import { useAssetStore } from "@/store";
 import { useEffect } from "react";
 
 export default function AssetsSync(props: { assetsSymbols: string[] }) {
   const { assetsSymbols } = props;
+  const changeAsset = useAssetStore((state) => state.changeAsset);
 
   useEffect(() => {
     socket.connect();
 
     socket.emit("joinAssets", { symbols: assetsSymbols });
     socket.on("assets/price-changed", (asset: Asset) => {
-      console.log(asset);
+      console.log("Preço mudou: ", asset);
+      changeAsset(asset);
     });
 
     // na descontrucao do componente, desfaz acoes de inicializacao
@@ -20,7 +23,7 @@ export default function AssetsSync(props: { assetsSymbols: string[] }) {
       socket.emit("leaveAssets", { symbols: assetsSymbols });
       socket.off("assets/price-changed");
     };
-  }, [assetsSymbols]);
+  }, [assetsSymbols, changeAsset]);
 
   return null;
 }
