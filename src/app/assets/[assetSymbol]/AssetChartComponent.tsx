8479d3be2513113ae3405b9c3790e57a6fb689bd
Alexsander <alexsander1996@yahoo.com.br>
2025-03-05 20:41:15 -0300

    conclusao aula 03
    - Transformado o componente do gráfico com dados dinâmicos;
    - Criação do tipo dos diários do ativos;
    - Adicionado query de consulta aos diários do ativo;

--- src/app/assets/[assetSymbol]/AssetChartComponent.tsx
+++ src/app/assets/[assetSymbol]/AssetChartComponent.tsx
@@ -1,14 +1,36 @@
-'use client';
+"use client";
 
 import { AssetShow } from "@/components/AssetShow";
 import { ChartComponent, ChartComponentRef } from "@/components/ChartComponent";
 import { Asset } from "@/models";
-import { useRef } from "react";
+import { socket } from "@/socket-io";
+import { Time } from "lightweight-charts";
+import { useEffect, useRef } from "react";
 
-export default function AssetChartComponent(props: { asset: Asset }) {
+export default function AssetChartComponent(props: {
+  asset: Asset;
+  data?: { time: Time; value: number }[];
+}) {
   const chartRef = useRef<ChartComponentRef>(null);
+  const symbol = props.asset.symbol; // desestruturamos para evitar re-renderizacoes que o useeffect causaria se recebessemos props...
+
+  useEffect(() => {
+    socket.connect();
+    socket.emit("joinAsset", { symbol });
+    socket.on("assets/daily-created", (assetDaily) => {
+      console.log("AssetDaily: ", assetDaily);
+      chartRef.current?.update({
+        time: (Date.parse(assetDaily.date) / 1000) as Time,
+        value: assetDaily.price,
+      });
+    });
+  }, [symbol]);
 
   return (
-    <ChartComponent header={<AssetShow asset={props.asset} />} ref={chartRef} />
+    <ChartComponent
+      header={<AssetShow asset={props.asset} />}
+      ref={chartRef}
+      data={props.data}
+    />
   );
 }
--- src/queries/queries.tsx
+++ src/queries/queries.tsx
@@ -1,4 +1,4 @@
-import { Asset, Order, Wallet } from "@/models";
+import { Asset, AssetDaily, Order, Wallet } from "@/models";
 
 export async function getAssets(): Promise<Asset[]> {
   // recomendado o uso do fetch pois tem melhorias feitas pela Vercel
@@ -21,6 +21,15 @@ export async function getOrders(walletId: string): Promise<Order[]> {
   // recomendado o uso do fetch pois tem melhorias feitas pela Vercel
   const response = await fetch(
     `http://localhost:3000/orders?walletId=${walletId}`
+  );
+  return response.json();
+}
+
+export async function getAssetDailies(
+  assetSymbol: string
+): Promise<AssetDaily[]> {
+  const response = await fetch(
+    `http://localhost:3000/assets/${assetSymbol}/dailies`
   );
   return response.json();
 }
